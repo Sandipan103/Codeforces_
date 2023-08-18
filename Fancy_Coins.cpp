516cb28dd76080b87928a5e2655e6015f8784a50
Sandipan19000 <sarkarsandipan19000@gmai.com>
2023-08-18 21:26:03 +0530

    first

---
+++ Fancy_Coins.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define pob pop_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<ll>
+#define vvl vector<vl>
+#define vs vector<string>
+#define vvs vector<vs>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define all(v) v.begin(), v.end()
+#define pii pair<int, int>
+#define pci pair<char, int>
+#define f(i, s, e)  for(int i=s; i<e; i++)
+#define fr(i, e, s)  for(int i=e; i>=s; i--)
+#define yy cout << "YES\n"
+#define nn cout << "NO\n"
+ll mod = 1e9+7;
+
+ll mul(ll a, ll b)  {return ((a%mod) *1ll* (b%mod)) %mod;}
+int gcd(int a, int b)   {return __gcd(a, b); }
+ll lcm(ll a, ll b)  {ll g = gcd(a%mod, b%mod); return (a*1ll*b)/g;}
+
+
+
+
+
+void Sandipan()    {
+    
+    ll n, k, a, b;
+    cin >> n >> k >> a >> b;
+    ll mini = min(n/k, b);
+    n -= (mini*k);
+    if(n <= a)  {
+        cout << "0\n";
+        return;
+    }
+
+    ll cnt = n/k;
+    n -= k*(n/k);
+    if(n > a)   {
+        cnt += (n-a);
+        cout << cnt << endl;
+        return;
+    }
+    a -= n;
+    cnt -= (a/k);
+
+    cout << cnt << endl;
+
+}
+
+
+
+int main()  {
+  int testCase;
+  cin >> testCase;
+  
+  while(testCase--)
+
+    Sandipan();
+  
+  return 0;
+}
