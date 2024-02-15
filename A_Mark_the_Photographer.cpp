de82c3309d9b37e182384cb1a0db3ea317c6eded
Sandipan19000 <sarkarsandipan19000@gmail.com>
2024-02-15 19:43:52 +0530

    practice

---
+++ A_Mark_the_Photographer.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+#define pb push_back
+#define pob pop_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vc vector<char>
+#define vvc vector<vc>
+#define vl vector<ll>
+#define vvl vector<vl>
+#define vs vector<string>
+#define vvs vector<vs>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define all(v) v.begin(), v.end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pci pair<char, int>
+#define f(i, s, e)  for(int i=s; i<e; i++)
+#define fr(i, e, s)  for(int i=e; i>=s; i--)
+#define yy cout << "YES\n"
+#define nn cout << "NO\n"
+ll mod = 1e9+7;
+
+ll mul(ll a, ll b)  {return ((a%mod) *1ll* (b%mod)) %mod;}
+int gcd(int a, int b)   {return __gcd(a, b); }
+ll lcm(ll a, ll b)  {ll g = gcd(a%mod, b%mod); return (a*1ll*b)/g;}
+
+
+
+
+
+void Sandipan()    {
+    
+    int n, k;
+    cin >> n >> k;
+    vi v(2*n);
+    f(i, 0, 2*n)  cin >> v[i];
+    sort(all(v));
+    int i = 0, j = n, f = 1;
+    while(i < n) {
+        if(v[j] - v[i] < k) f = 0;
+        i++; j++;
+    }
+    if(f)   yy;
+    else    nn;
+    
+}
+
+
+
+int main()  {
+  int testCase;
+  cin >> testCase;
+  
+  while(testCase--)
+
+    Sandipan();
+  
+  return 0;
+}
