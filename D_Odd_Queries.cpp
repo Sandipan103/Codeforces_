16485eed70f66bea1e827a1cbdb559d392b4b54d
Sandipan19000 <sarkarsandipan19000@gmail.com>
2024-02-16 11:38:05 +0530

    Round 859 div-4

---
+++ D_Odd_Queries.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+#define pb push_back
+#define pob pop_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vc vector<char>
+#define vvc vector<vc>
+#define vl vector<ll>
+#define vvl vector<vl>
+#define vs vector<string>
+#define vvs vector<vs>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define all(v) v.begin(), v.end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pci pair<char, int>
+#define f(i, s, e)  for(int i=s; i<e; i++)
+#define fr(i, e, s)  for(int i=e; i>=s; i--)
+#define yy cout << "YES\n"
+#define nn cout << "NO\n"
+ll mod = 1e9+7;
+
+ll mul(ll a, ll b)  {return ((a%mod) *1ll* (b%mod)) %mod;}
+int gcd(int a, int b)   {return __gcd(a, b); }
+ll lcm(ll a, ll b)  {ll g = gcd(a%mod, b%mod); return (a*1ll*b)/g;}
+
+
+
+
+
+void Sandipan()    {
+    
+    ll n, q, l, r, k;
+    cin >> n >> q;
+    vl v(n), pref(n, 0);
+    f(i, 0, n)  cin >> v[i];
+    pref[0] = v[0];
+    f(i, 1, n)    pref[i] = pref[i-1] + v[i];
+
+    f(i, 0, q)  {
+        cin >> l >> r >> k;
+        l--, r--;
+        ll total = pref[n-1] - pref[r];
+        if(l)   total += pref[l-1];
+        total += (r-l+1) * k;
+        if(total&1) yy;
+        else    nn;
+    }
+}
+
+
+
+int main()  {
+  int testCase;
+  cin >> testCase;
+  
+  while(testCase--)
+
+    Sandipan();
+  
+  return 0;
+}
---
+++ E_Interview.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+#define pb push_back
+#define pob pop_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define vc vector<char>
+#define vvc vector<vc>
+#define vl vector<ll>
+#define vvl vector<vl>
+#define vs vector<string>
+#define vvs vector<vs>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define all(v) v.begin(), v.end()
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pci pair<char, int>
+#define f(i, s, e)  for(int i=s; i<e; i++)
+#define fr(i, e, s)  for(int i=e; i>=s; i--)
+#define yy cout << "YES\n"
+#define nn cout << "NO\n"
+ll mod = 1e9+7;
+
+ll mul(ll a, ll b)  {return ((a%mod) *1ll* (b%mod)) %mod;}
+int gcd(int a, int b)   {return __gcd(a, b); }
+ll lcm(ll a, ll b)  {ll g = gcd(a%mod, b%mod); return (a*1ll*b)/g;}
+
+
+
+
+
+void Sandipan()    {
+    
+    int n;
+    cin >> n;
+    vi v(n+1), pref(n+1, 0);
+    f(i, 1, n+1)  cin >> v[i], pref[i] = pref[i-1] + v[i];
+    int ans, s = 1, e = n, temp;
+    while(s <= e)   {
+        int mid = s + (e - s) / 2;
+        cout << "? " << mid - s + 1 << " ";
+        f(j, s, mid+1)    cout << j << " ";
+        cout << endl;
+        cin >> temp;
+        if(temp > pref[mid] - pref[s-1])  ans = mid, e = mid - 1;
+        else    s = mid + 1;
+    }
+    cout << "! " << ans <<  endl;
+    cout.flush();
+
+}
+
+
+
+int main()  {
+  int testCase;
+  cin >> testCase;
+  
+  while(testCase--)
+
+    Sandipan();
+  
+  return 0;
+}
